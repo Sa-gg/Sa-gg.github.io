c3d339c021114a44393826fa74d4c7f7b47bdcc9
Patrick R. Sagum <sagum.patrick.r@gmail.com>
2024-02-06 04:29:20 +0800

    added function (not-final)

--- simple-todo-list/app.js
+++ simple-todo-list/app.js
@@ -4,9 +4,37 @@ let taskContainer = document.querySelector(".tasks-container")
 
 
 addTaskBtn.addEventListener("click", function() {
-    let newTask = document.createElement("div");
-    newTask.textContent = newTaskText.value
+    let newTask = document.createElement("div")
+    newTask.classList.add("task")
+    newTask.innerHTML = 
+    `<p>${newTaskText.value}</p>
+    <div class="checkDeleteTask">
+        <i class="fa-solid fa-check check"></i>
+        <i class="fa-solid fa-trash-can remove"></i>
+    </div>`
     taskContainer.appendChild(newTask)
-    console.log(newTask)
     newTaskText.value = ""
 })
+
+
+
+taskContainer.addEventListener("click", function(event) {
+    const target = event.target;
+
+    if (target.classList.contains("check")) {
+        const taskText = target.parentNode.previousElementSibling;
+
+        if (taskText.style.textDecoration === "line-through") {
+            taskText.style.textDecoration = "none";
+        } else {
+            taskText.style.textDecoration = "line-through";
+        }
+    }
+
+
+    if (target.classList.contains("remove")) {
+        target.parentNode.parentNode.remove()
+    }
+    
+
+});
