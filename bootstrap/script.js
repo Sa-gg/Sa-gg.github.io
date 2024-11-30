195a87a4f2c48a5e467ebf6a68caaf87e4503e01
Patrick Sagum <sagum.patrick.r@gmail.com>
2024-11-30 11:52:14 +0800

    Added Services & Footer Section
    Co-authored-by: Jegrick Sildora <jegs-sildora@users.noreply.github.com>

---
+++ bootstrap/script.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    const navbar = document.querySelector('.navbar');
+    const navLinks = document.querySelectorAll('.navbar .nav-item > .nav-link');
+    const searchInput = document.getElementById('search-input');
+  
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
+        navbar.classList.remove('bg-transparent');
+        navbar.classList.add('bg-body-tertiary');
+  
+        navLinks.forEach(link => {
+          link.style.color = 'black';
+        });
+
+        searchInput.style.border = '2px solid orange';
+      } else {
+        navbar.classList.remove('bg-body-tertiary');
+        navbar.classList.add('bg-transparent');
+  
+        navLinks.forEach(link => {
+          link.style.color = 'white';
+        });
+
+        searchInput.style.borderColor = '';
+      }
+    });
+  };
+  
