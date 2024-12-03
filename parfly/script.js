a4b4119ed5a81aa22e32d4154544dadd5124d714
Patrick Sagum <sagum.patrick.r@gmail.com>
2024-12-03 11:17:16 +0800

    Updated Parfly Responsiveness

--- parfly/script.js
+++ parfly/script.js
@@ -1,28 +1,64 @@
 window.onload = function () {
-    const navbar = document.querySelector('.navbar');
-    const navLinks = document.querySelectorAll('.navbar .nav-item > .nav-link');
-    const searchInput = document.getElementById('search-input');
-  
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 0) {
-        navbar.classList.remove('bg-transparent');
-        navbar.classList.add('bg-body-tertiary');
-  
-        navLinks.forEach(link => {
-          link.style.color = 'black';
-        });
+  const navbar = document.querySelector('.navbar');
+  const navLinks = document.querySelectorAll('.navbar .nav-item > .nav-link');
+  const searchInput = document.getElementById('search-input');
+  const menuBtn = document.getElementById("menu-btn");
 
-        searchInput.style.border = '3px solid orange';
-      } else {
-        navbar.classList.remove('bg-body-tertiary');
-        navbar.classList.add('bg-transparent');
-  
-        navLinks.forEach(link => {
-          link.style.color = 'white';
-        });
 
-        searchInput.style.border = '3px solid black';
-      }
-    });
-  };
   
+
+  menuBtn.addEventListener("click", () => {
+
+     
+    if(navbar.classList.contains("bg-body-tertiary")) {
+      navbar.classList.remove("bg-body-tertiary")
+      navbar.classList.add("bg-transparent")
+
+      navLinks.forEach(link => {
+
+        link.style.color = "white"
+      })
+
+      searchInput.style.border = '3px solid white';
+
+    } else {
+      navbar.classList.remove('bg-transparent');
+      navbar.classList.add('bg-body-tertiary');
+
+      
+      navLinks.forEach(link => {
+        link.style.color = 'black';
+      });
+
+      searchInput.style.border = '3px solid orange';
+    }
+
+
+  
+
+
+
+  })
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      navbar.classList.remove('bg-transparent');
+      navbar.classList.add('bg-body-tertiary');
+
+      navLinks.forEach(link => {
+        link.style.color = 'black';
+      });
+
+      searchInput.style.border = '3px solid orange';
+    } else {
+      navbar.classList.remove('bg-body-tertiary');
+      navbar.classList.add('bg-transparent');
+
+      navLinks.forEach(link => {
+        link.style.color = 'white';
+      });
+
+      searchInput.style.border = '3px solid white';
+    }
+  });
+};
