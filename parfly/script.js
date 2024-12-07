65c5cf3fccd1a67151cdbb40bd11cfb4e299f60e
Patrick Sagum <sagum.patrick.r@gmail.com>
2024-12-07 20:51:19 +0800

    Parfly Version1

--- parfly/script.js
+++ parfly/script.js
@@ -1,64 +1,36 @@
-window.onload = function () {
+window.onload = () => {
   const navbar = document.querySelector('.navbar');
-  const navLinks = document.querySelectorAll('.navbar .nav-item > .nav-link');
-  const searchInput = document.getElementById('search-input');
+  const navLinks = document.querySelectorAll('.navbar .nav-item > .nav-link:not(.signup)');
   const menuBtn = document.getElementById("menu-btn");
+  const collapseContent = document.getElementById("navbarSupportedContent");
+  const globeIcon = document.querySelector('.fa-globe');
+  const userIcon = document.querySelector('.bx-user');
 
+  const toggleNavbar = (isTransparent) => {
+    navbar.classList.toggle('bg-body-tertiary', !isTransparent);
+    navbar.classList.toggle('bg-transparent', isTransparent);
 
-  
+    const textColor = isTransparent ? 'white' : 'black';
+    globeIcon.style.color = isTransparent ? 'white' : 'orange';
+    userIcon.style.color = isTransparent ? 'white' : 'orange';
+
+    navLinks.forEach(link => {
+      if (link.classList.contains('lang') || link.classList.contains('login')) {
+        link.style.color = isTransparent ? 'white' : 'orange'; 
+      } else {
+        link.style.color = textColor;
+      }
+    });
+  };
 
   menuBtn.addEventListener("click", () => {
-
-     
-    if(navbar.classList.contains("bg-body-tertiary")) {
-      navbar.classList.remove("bg-body-tertiary")
-      navbar.classList.add("bg-transparent")
-
-      navLinks.forEach(link => {
-
-        link.style.color = "white"
-      })
-
-      searchInput.style.border = '3px solid white';
-
-    } else {
-      navbar.classList.remove('bg-transparent');
-      navbar.classList.add('bg-body-tertiary');
-
-      
-      navLinks.forEach(link => {
-        link.style.color = 'black';
-      });
-
-      searchInput.style.border = '3px solid orange';
+    if (window.scrollY === 0 && !collapseContent.classList.contains("show")) {
+      toggleNavbar(navbar.classList.contains('bg-body-tertiary'));
     }
-
-
-  
-
-
-
-  })
+  });
 
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 0) {
-      navbar.classList.remove('bg-transparent');
-      navbar.classList.add('bg-body-tertiary');
-
-      navLinks.forEach(link => {
-        link.style.color = 'black';
-      });
-
-      searchInput.style.border = '3px solid orange';
-    } else {
-      navbar.classList.remove('bg-body-tertiary');
-      navbar.classList.add('bg-transparent');
-
-      navLinks.forEach(link => {
-        link.style.color = 'white';
-      });
-
-      searchInput.style.border = '3px solid white';
-    }
+    const isTransparent = window.scrollY === 0 && !collapseContent.classList.contains("show");
+    toggleNavbar(isTransparent);
   });
 };
